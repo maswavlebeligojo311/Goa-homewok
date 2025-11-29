44b8ced5445b8dcc3abdad8e967f2d998b3df62c
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2025-11-29 14:16:35 -0500

    commit

--- Level 001/homework/homework.py
+++ Level 001/homework/homework.py
@@ -2,18 +2,18 @@ Name = "Nikoloz"
 print (Name)
 Lastname = "Gelashvili"
 print (Lastname)
-Age = "14"
+Age = 14
 print (Age)
 Favoritecar = "Buggati"
 print (Favoritecar)
 
 
 
-Number = "100"
+Number = 100
 print (Number)
-Number = "1000"
+Number = 1000
 print (Number)
-Number = "10000"
+Number = 10000
 print (Number)
 
-#ჩვენ პიტონს ვიყენებთ, რადგან პითონი არის ყველაზე მარტივი და დამწყებებისტვის მარტივი გამოსაყენებელი კოდობა
+#ჩვენ პიტონს ვიყენებთ, რადგან პითონი არის ყველაზე მარტივი და დამწყებებისტვის მარტივი გამოსაყენებელი პროგრამირების ენა.
