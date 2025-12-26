6646cc0e9aaf867f91198bc6b52e56729cffe4c5
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2025-12-26 11:54:29 -0500

    commit

--- Level 009/Classwork/Classwork.py
+++ Level 009/Classwork/Classwork.py
@@ -1,3 +1,12 @@
+#input ბრძანება რომელიც გამოიყენება კომპიუტერში ინპორმაციის შესანახათ
+
+
+Reality = input("Learning is good:")
+Reality2 = input("Don't judge a book by it's cover:")
+Reality3 = input("If you try enaugh, you will do anything")
+
+
+
 My_Name = input("My name is:")
 print (My_Name)
 
@@ -5,4 +14,8 @@ print (My_Name)
 
 My_age = int(input("My age is:"))
 My_Dads_age = int(input("My dads age is:"))
-print (My_age - My_Dads_age)
+print (My_Dads_age - My_age)
+
+
+My_Lastname = input("My lastname is:")
+print (My_Lastname)
---
+++ Level 009/Homework/Homework.py
@@ -0,0 +1,21 @@
+My_name = input("My name is:")
+My_Lastname = input("My lastname is:")
+print (My_name + My_Lastname)
+
+
+#input და output შორის განსხვავება არის ის რომ Input გამოიყენება ღილაკში ინფორმაციის შესანახათ, როდესაც outputს ის გამოაქვს ეკრანზე, და გამოიყენება ხმის გასაკეთებლად.
+
+
+My_balance = int(input("My balance is:"))
+My_expenses1 = int(input("My expenses is:"))
+print (My_balance - My_expenses1)
+My_expenses2 = int(input("My expenses is:"))
+print (My_balance - My_expenses2)
+My_expenses3 = int(input("My expenses is:"))
+print (My_balance - My_expenses3)
+
+
+
+
+
+print (My_balance - My_expenses3 - My_expenses2 - My_expenses1)
