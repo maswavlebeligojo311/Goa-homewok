a9bf5120507df499be506646ddfd087977fc1136
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2026-01-03 12:03:34 -0500

    commit

---
+++ Level 011/Homework/Homework.py
@@ -0,0 +1,25 @@
+Number1 =   int(input (Number1 is:))
+Number2 =  int(input (number2 is:)) 
+Number3 =  int((input Number3 is:))
+print (Number1 + Number2 + Number3)
+
+
+Name1 = input(Name1 is:)
+Name2 = input(Name2 is:)
+Name3 = input(Name3 is:)
+print (Name1 + Name2 + Name3)
+
+
+Num1 = float(input(Num1 is:))
+Num2 = float(input(Num2 is:))
+Num3 = float(input(Num3 is:))
+print (Num1 + Num2 + Num3)
+
+
+
+#Type გამოიყენებამ იმისათვის რომ გავარკვიოთ ჩვენი კოდის ტიპი.
+
+My_name = input(My name is:)
+My_lastname = input(My lastname is:)
+print (My_name + My_lastname)
+type(print (My_name + My_lastname))
